e7662bf7a84c14fec2f09114797b0503497c8116
[adriancelistorres] <[adriancelistorres184@gmail.com]>
2022-07-15 22:32:18 -0500

    agregue login y readme

--- service/login.php
+++ service/login.php
@@ -1,4 +1,7 @@
 <?php
+  //1 error de conexion
+    //2 email invalido
+    //2 Contraseña incorrecta
 include('conexion.php');
 $emausu=$_POST['emausu'];
 $sql="Select * from usuario where emausu='$emausu' ";
@@ -8,8 +11,24 @@ $result=mysqli_query($con,$sql);
 if($result){
     $row=mysqli_fetch_array($result);
     $count=mysqli_num_rows($result);
+    if($count!=0){
+        $pasusu=$_POST['pasusu'];
+        if($row['pasusu']!=$pasusu){  
+        header('Location: ../login.php?e=3');
+        }else{
+            session_start();
+            $_SESSION['codusu']=$row['codusu'];
+            $_SESSION['emausu']=$row['emausu'];
+            $_SESSION['nomusu']=$row['nomusu'];
+            header('Location: ../');
+        }
+    }
+    else{
+        header('Location: ../login.php?e=2');
+    }
     
 }else{
     //1 error de conexion
-    header('Location:../login.php?e=1');
+    header('Location: ../login.php?e=1');
 }
+
